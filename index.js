858aab4a4152555f56224788852be4e48eec785c
Elizaveta <elizaveta.battalova@rambler.ru>
2022-04-13 02:40:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
-$(document).ready(function() {
-    $("#menuButton").click(function() {
-        $(".otherButton1").toggle(500);
-        $(".otherButton2").toggle(700);
-        $(".otherButton3").toggle(900);
-    });
-    $("#menuButton").click(function() {
-        $('.flip').toggleClass('flip-active');
-        if ($('.flip').hasClass('flip-active')) {
-            $('.flip').addClass('fa-times');
-            $('.flip').removeClass('fa-volume-control-phone');
-        } else {
-            $('.flip').addClass('fa-volume-control-phone');
-            $('.flip').removeClass('fa-times');
+const dropdowns = Array.from(document.querySelectorAll('.dropdown'));
+dropdowns.forEach(dropdown => {
+    const btn = dropdown.querySelector('.dropdown-btn');
+    const menu = dropdown.querySelector('.dropdown-menu');
+
+    document.addEventListener('click', (e) => {
+        if (!dropdown.contains(e.target)) {
+            menu.dataset.expanded = "false";
         }
+    })
+
+    btn.addEventListener('click', (e) => {
+        if (menu.dataset.expanded === "true") {
+            menu.dataset.expanded = "false";
+        } else {
+            menu.dataset.expanded = "true";
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('#biografy-btn').click(function() {
+        $('#sidebar-biography').toggleClass('visible');
+    });
+});
+
+$(document).ready(function() {
+    $('#biography-sidebar-btn').click(function() {
+        $('#sidebar-biography').toggleClass('visible');
+    });
+});
+
+
+$(document).ready(function() {
+    $('#filmography-btn').click(function() {
+        $('#sidebar-filmography').toggleClass('visible');
+    });
+});
+$(document).ready(function() {
+    $('#filmography-sidebar-btn').click(function() {
+        $('#sidebar-filmography').toggleClass('visible');
+    });
+});
+
+
+$(document).ready(function() {
+    $('#video-btn').click(function() {
+        $('#sidebar-video').toggleClass('visible');
+    });
+});
+$(document).ready(function() {
+    $('#video-sidebar-btn').click(function() {
+        $('#sidebar-video').toggleClass('visible');
     });
 });
